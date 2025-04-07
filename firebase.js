f787c901d480bbc3ff601382df97f16d0b7c213e
Priya-150 <159451684+Priya-150@users.noreply.github.com>
2025-04-06 21:46:16 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebas
 import { getDatabase, ref, onValue, set, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBBWQ9QSs0eLQql_j1WOEcdVteIjpaJil4",
+  authDomain: "smart-car-parking-37358.firebaseapp.com",
+  projectId: "smart-car-parking-37358",
+  storageBucket: "smart-car-parking-37358.firebasestorage.app",
+  messagingSenderId: "130651437461",
+  appId: "1:130651437461:web:5db5ae2903d6cc52ed1e9c",
+  measurementId: "G-DVJHJ0HRBB"
 };
 
 const app = initializeApp(firebaseConfig);
